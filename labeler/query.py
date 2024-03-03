eb5b03447f5daa56eabd0b65c546cb9758a6b0f9
Binh Vu <binh@toan2.com>
2024-03-03 09:13:12 -0800

    add labeled data

--- labeler/query.py
+++ labeler/query.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from functools import partial
 from pathlib import Path
 
+import pandas as pd
 import serde.json
 import streamlit as st
 import streamlit.components.v1 as components
@@ -57,3 +58,103 @@ WHERE {
                  """
 )
 # %%
+
+run_minmod_query(
+    """
+SELECT *
+WHERE {
+    ?s :record_id 10067592 ;
+                 :deposit_type_candidate [ ?p ?o ] .
+}
+                 LIMIT 1000
+"""
+)
+# %%
+
+
+run_minmod_query(
+    """
+SELECT ?ms ?oms ?dt_conf ?dt_s ?cn ?cg ?ce ?country ?dtnorm ?dtnorm_label ?loc_wkt
+WHERE {
+    ?ms a :MineralSite .
+    ?ms :deposit_type_candidate ?dpc .
+    ?dpc :confidence ?dt_conf .
+    ?dpc :source ?dt_s .
+    
+    OPTIONAL { ?ms owl:sameAs ?oms . }
+
+    ?dpc :normalized_uri ?dtnorm .
+    ?dtnorm rdfs:label ?dtnorm_label .
+    ?ms :location_info [ :country ?country ] .
+
+    OPTIONAL {
+        ?ms :location_info [ :location ?loc_wkt ] .
+        FILTER(datatype(?loc_wkt) = geo:wktLiteral)
+    }
+}"""
+)
+
+# %%
+
+df = run_minmod_query(
+    """
+SELECT DISTINCT ?ms ?msname # ?cat ?ore
+WHERE {
+    ?ms a :MineralSite ;
+        rdfs:label|:name ?msname .
+    ?ms :deposit_type_candidate ?dpc .
+
+    ?ms :mineral_inventory ?mi .
+
+    ?mi :ore [ :ore_value ?ore ] ;
+        :category ?cat .
+
+    ?dpc :confidence ?dt_conf .
+    ?dpc :source 'expert' .
+    
+    # OPTIONAL { ?ms owl:sameAs ?oms . }
+
+    # ?dpc :normalized_uri ?dtnorm .
+    # ?dtnorm rdfs:label ?dtnorm_label .
+    # ?ms :location_info [ :country ?country ] .
+
+    # OPTIONAL {
+    #     ?ms :location_info [ :location ?loc_wkt ] .
+    #     FILTER(datatype(?loc_wkt) = geo:wktLiteral)
+    # }
+}"""
+)
+
+# %%
+
+uris = set(df["ms.value"].unique())
+
+# %%
+
+df3 = pd.read_csv("/Users/rook/Downloads/flat_mineral_site_data.v3.csv")
+df2 = pd.read_csv("/Users/rook/Downloads/filtered_nickel_us.0216.csv")
+# %%
+
+
+run_minmod_query(
+    """
+SELECT ?ms ?msname
+WHERE {
+    ?ms a :MineralSite ;
+        rdfs:label ?msname .
+    ?ms :deposit_type_candidate ?dpc .
+    ?dpc :confidence ?dt_conf .
+    ?dpc :source 'expert' .
+    
+    # OPTIONAL { ?ms owl:sameAs ?oms . }
+
+    # ?dpc :normalized_uri ?dtnorm .
+    # ?dtnorm rdfs:label ?dtnorm_label .
+    # ?ms :location_info [ :country ?country ] .
+
+    # OPTIONAL {
+    #     ?ms :location_info [ :location ?loc_wkt ] .
+    #     FILTER(datatype(?loc_wkt) = geo:wktLiteral)
+    # }
+}"""
+)
